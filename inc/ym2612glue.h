f66be8e6841981b29e12ca744158c9f6d779832d
Matthieu Freeman <me@ilambda.io>
2016-11-03 17:13:25 +0100

    Exported helper methods to new file

--- inc/ym2612glue.h
+++ inc/ym2612glue.h
@@ -2,6 +2,8 @@
 #define NULL 0
 #endif
 
+#include "ym2612action.h"
+
 static unsigned char* cmdbus = NULL;
 static unsigned char* databus = NULL;
 static unsigned char* datadir = NULL;
@@ -9,11 +11,4 @@ static unsigned char* datadir = NULL;
 static unsigned char selreg = 0;
 
 void ym2612_init(unsigned char* cmdbus, unsigned char* databus, unsigned char* datadir);
-
-
-void ym2612_select(unsigned char addr, unsigned char part);
-void ym2612_write(unsigned char addr, unsigned char part);
-unsigned char ym2612_read(unsigned char addr, unsigned char part);
-
-unsigned char ym2612_getreg(unsigned char addr, unsigned char part);
-void ym2612_setreg(unsigned char addr, unsigned char regval, unsigned char part);
+void ym2612_do(ymaction_t action);
